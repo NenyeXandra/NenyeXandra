da8562b20c86081ee741d592a3e36d1975735ba8
Nenye Sandra <145860461+NenyeXandra@users.noreply.github.com>
2023-09-23 17:40:31 +0100

    Add files via upload

---
+++ CHINENYE'S CODE 1.py
@@ -0,0 +1,7 @@
+print("Chinenye Sandra Esomonu")
+print("September 23")
+print ("""I have signed up for Replit's 100 days of Python challenge!
+I will make sure to spend some time every day coding along, for a minimum of 10 minutes a day.
+I'll be using Replit, an amazing online IDE so I can do this from my phone wherever I happend to be.
+No excuses for not coding from the middle of a field!
+I am feeling happy You can follow my progress at replit.com/@chinenyesandra9""")
